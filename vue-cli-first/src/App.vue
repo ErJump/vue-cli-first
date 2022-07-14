dfed8340a0abf558279f8d53ccf24d338f3638d5
ErJump <fedelijunior@gmail.com>
2022-07-14 14:09:25 +0200

    feat: :sparkles: Adds HeroMain
    Adds hero main, removes hello world

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeroMain/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroMain from './components/HeroMain.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeroMain
   }
 }
 </script>
---
+++ vue-cli-first/src/components/HeroMain.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hero">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 hero">
+          <h1>IS THAT THE REAL VUE?</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HeroMain',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .hero {
+    background-image: url(https://www.icegif.com/wp-content/uploads/2022/03/icegif-1061.gif);
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    height: 70vh;
+  }
+  .hero>*{
+    color: #fff;
+    font-size: 50px;
+    text-shadow: 3px 3px #000;
+    transform: translateY(20px);
+  }
+</style>
